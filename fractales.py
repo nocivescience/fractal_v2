9a04da7fce4ec5888db8c5756b4c78cfc13a1105
noceive <nocivescience@gmail.com>
2022-09-30 03:30:06 -0300

    esto es importante

--- fractales.py
+++ fractales.py
@@ -11,10 +11,16 @@ class RecursionScene(Scene):
         line_2.move_to(line_1.get_end(),line_2.get_start())
         line_1.add(line_2)
         # self.play(*[
+        line_3=line_1.copy()
+        line_3.move_to(line_1.get_end(),line_3.get_start())
         #     Create(line) for line in [
         #         line_1,
         #         line_2
         #     ]
         # ])
-        self.play(Create(line_1))
+        for lin in [
+            line_1,
+            line_3
+        ]:
+            self.play(Create(lin))
         self.wait()
